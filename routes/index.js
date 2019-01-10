4da91a1b9aafec447858b1918793bdec16bd8e0d
jimmy <jimmy@JimmydeMacBook-Pro.local>
2019-01-11 01:06:21 +0800

    index.js working

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var router = express.Router();
 
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'root',
-  password : '123',
+  port 	   : '3306',
+  user     : 'jimmy',
+  password : 'jimmy',
   database : 'Anime',
   insecureAuth : true
 });
@@ -44,58 +45,129 @@ router.post('/AnimeSearch',function(req,res,next){
 		var AName = req.body.AnimeName.trim()
 		var queryString = "SELECT * FROM Animation WHERE AName = ?"
 		connection.query(queryString , AName , (err,results,fields)=>{
-			console.log("result = ",results)
-			console.log("length = ",results.length)
-			for(var i = 0 ; i < results.length ; i++){
-				results[i].image = 'images/anime/' + results[i].image
+			if(results){
+				console.log("result = ",results)
+				console.log("length = ",results.length)
+				for(var i = 0 ; i < results.length ; i++){
+					results[i].image = 'images/anime/' + results[i].image
+				}
+				res.send(results)
 			}
-			res.send(results)
+			else{
+				output = {
+					AnimeID : 'None'
+				}
+				res.send(output);
+			}
 		})
 	}
 	if(req.body.SearchField == "Genre"){
 		console.log("Genre = ",req.body.GenreName);
-		var QueryArr = req.body.GenreName;
-		console.log()
+		var QueryArr = [req.body.GenreName];
 		var RetField = ['Genre.AnimeID','AName','Episode','Season','Rating','AnimeDate','sdName','image','Genre.Genre']
 		var queryString = "SELECT DISTINCT " + RetField.join()+ " FROM Genre join Animation on Genre.AnimeID = Animation.AnimeID WHERE Genre.Genre in (" + QueryArr.join() + ")" 
 		connection.query(queryString , (err,results,fields)=>{
-			var AniMap = new Map();
-			var GenreList = ['None','動作','魔法','奇幻','懸疑','冒險','校園','戀愛','青春','音樂'];
-			var outputList = [];
-			// console.log("result = ",results)
-			// console.log("length = ",results.length)
-			for(var i = 0 ; i < results.length ; i++){
-				if(AniMap.get(results[i].AnimeID) != 1){
-					outputList.push(results[i]);
-					outputList[outputList.length-1].image = 'images/anime/' + results[i].image
-					outputList[outputList.length-1].Genre = GenreList[parseInt(results[i].Genre)];
-					AniMap.set(results[i].AnimeID,1)
+			if(results){
+				var AniMap = new Map();
+				var GenreList = ['None','動作','魔法','奇幻','懸疑','冒險','校園','戀愛','青春','音樂'];
+				var outputList = [];
+				// console.log("result = ",results)
+				// console.log("length = ",results.length)
+				for(var i = 0 ; i < results.length ; i++){
+					if(AniMap.get(results[i].AnimeID) != 1){
+						outputList.push(results[i]);
+						outputList[outputList.length-1].image = 'images/anime/' + results[i].image
+						outputList[outputList.length-1].Genre = GenreList[parseInt(results[i].Genre)];
+						AniMap.set(results[i].AnimeID,1)
+					}
+					else{
+						outputList[outputList.length-1].Genre = outputList[outputList.length-1].Genre+','+GenreList[parseInt(results[i].Genre)];
+					}
 				}
-				else{
-					outputList[outputList.length-1].Genre = outputList[outputList.length-1].Genre+','+GenreList[parseInt(results[i].Genre)];
-				}
+				res.send(outputList)
 			}
-			res.send(outputList)
+			else{
+				output = {
+					AnimeID : 'None'
+				}
+				res.send(output);
+			}
 		})
 	}
 	if(req.body.SearchField =='Studio'){
 		var StudioList = ['None','Ufotable','TYPE-MOON','Nitro+','MAGES.','WHITE FOX','A-1 Pictures','三次元','J.C.STAFF','ISSEN'];
 		var StudioChoice = StudioList[parseInt(req.body.StudioName)]
+		console.log("Studio choice = ",StudioChoice);
 		var queryString = "SELECT * FROM Animation WHERE sdName = ?"
 		connection.query(queryString , StudioChoice , (err,results,fields)=>{
-			console.log("result = ",results);
-			console.log("length = ",results.length);
-			for(var i = 0 ; i < results.length ; i++){
-				results[i].image = 'images/anime/' + results[i].image
+			if(results){
+				console.log("result = ",results);
+				console.log("length = ",results.length);
+				for(var i = 0 ; i < results.length ; i++){
+					results[i].image = 'images/anime/' + results[i].image
+				}
+				res.send(results)
 			}
-			res.send(results)
+			else{
+				output = {
+					AnimeID : 'None'
+				}
+				res.send(output);
+			}
 		})
 	}
 })
 
+router.post('/VoiceSearch',function(req,res,next){
+	if(req.body.SearchField == "VoiceName"){
+		var VName = req.body.VoiceName.trim();
+		var queryString = "SELECT * FROM VoiceActor WHERE VName = ?"
+		connection.query(queryString , VName , (err,results,fields)=>{
+			if(results){
+				console.log("result = ",results);
+				console.log("length = ",results.length);
+				for(var i = 0 ; i < results.length ; i++){
+					results[i].image = 'images/voiceactor/' + results[i].image
+				}
+				res.send(results);
+			}
+			else{
+				output = {VName : 'None'};
+				res.send(output);
+			}
+		})
+	}
+	if(req.body.SearchField == 'gender'){
+		var gender;
+		console.log("gender = ",gender)
+		if(req.body.Sex == '0'){
+			gender = 'TRUE'
+		}
+		else{
+			gender = 'FALSE'
+		}
+		var queryString = "SELECT * FROM VoiceActor WHERE Gender = ?"
+		connection.query(queryString , gender , (err,results,fields)=>{
+			if(results){
+				console.log("result = ",results);
+				console.log("length = ",results.length);
+				for(var i = 0 ; i < results.length ; i++){
+					results[i].image = 'images/voiceactor/' + results[i].image
+				}
+				res.send(results);
+			}
+			else{
+				output = {VName : 'None'};
+				res.send(output);
+			}
+		})
+	}
+})
+
+
 // router.post('/AnimeDetail',function(req,res,next){
-	
+// 	var AniName = req.body.AniName;
+// 	var SearchField = req.body.SearchField;
 // })
 
-router.post
 module.exports = router;
